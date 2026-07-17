fe4fd2f010394521bd7a976cd128afd391cec28a
kvadrakola <kvadrakola@gmail.com>
2026-07-18 00:11:41 +0200

    add if and else

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 const list1 = [ true,  true,  true,  false, true,  true,  true,  true , true,  false, true,  false, true,  false, false, true , true,  true,  true,  true , false, false, true,  true ];
 const list2 = [ false, false, false ];
 
-const resultList1 = list1.filter((word) => word === true);  
+const resultList1 = list1.filter((word) => word === true); 
+const count1 = resultList1.length; 
 
-
-console.log(resultList1);
-
+if const1 === true {
+console.log("1) There are " + count1 + " sheep in total");
+}
+else {
+    console.log("1) UPS!!! Wolves have eaten the sheep");
+}
 
 
 
