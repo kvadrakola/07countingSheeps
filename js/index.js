88a9f8f004188998ce02a7226316f4b95c303266
kvadrakola <kvadrakola@gmail.com>
2026-07-17 23:47:55 +0200

    add some js code for test, connect js fili to html

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const list1 = [ true,  true,  true,  false, true,  true,  true,  true , true,  false, true,  false, true,  false, false, true , true,  true,  true,  true , false, false, true,  true ];
+const list2 = [ false, false, false ];
+
+const resultList1 = list1.filter((word) => word.length = 4);  
+
+
+console.log(resultList1);
+
+
+
+
+
