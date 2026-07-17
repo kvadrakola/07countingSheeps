e0b84add6260efffc31ed27609f8e0a9c42161eb
kvadrakola <kvadrakola@gmail.com>
2026-07-18 00:54:12 +0200

    add output for html page

--- js/index.js
+++ js/index.js
@@ -18,6 +18,12 @@ else {
 countSheep(list1, "1.");
 countSheep(list2, "2.");
 
+const output1 = countSheep(list1, "List 1:");
+const output2 = countSheep(list2, "List 2:");
+
+document.getElementById('result-display').innerHTML =
+output1 + "<br>" + output2;
+
 
 
 
