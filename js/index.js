cf103626ef993c7a3e06e2b105d9138aef30b91c
kvadrakola <kvadrakola@gmail.com>
2026-07-17 23:58:52 +0200

    fix: index.html add </script>, js fix code

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const list1 = [ true,  true,  true,  false, true,  true,  true,  true , true,  false, true,  false, true,  false, false, true , true,  true,  true,  true , false, false, true,  true ];
 const list2 = [ false, false, false ];
 
-const resultList1 = list1.filter((word) => word.length = 4);  
+const resultList1 = list1.filter((word) => word === true);  
 
 
 console.log(resultList1);
