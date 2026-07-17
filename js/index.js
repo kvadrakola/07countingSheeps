91d5cba85e9c9e7a3669dfaef1cf2a56b50370b8
kvadrakola <kvadrakola@gmail.com>
2026-07-18 00:15:05 +0200

    fix syntax errors, fix logic errors

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const list2 = [ false, false, false ];
 const resultList1 = list1.filter((word) => word === true); 
 const count1 = resultList1.length; 
 
-if const1 === true {
+if (count1 > 0) {
 console.log("1) There are " + count1 + " sheep in total");
 }
 else {
